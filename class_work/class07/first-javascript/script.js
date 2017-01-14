1ce20c7c6ef8ca2b9dd4ff2d791e5d8573e054ac
meganmanata <meganmanata@gmail.com>
2017-01-13 20:08:38 -0500

    class 8 work mish-mash + rgb homework
    I always forget to commit in class!

---
+++ class_work/class07/first-javascript/script.js
@@ -0,0 +1,25 @@
+document.write("Writing to document");
+
+var firstNum=10*10;
+var myName="My name is Coolio"
+
+document.write(firstNum);
+// document.write(100/10);
+// document.write(50-17);
+// document.write(50+17);
+document.write(myName);
+
+// alert("this sucks");
+
+// function stupidAlert() {
+// 	alert('stupid');
+// 	alert('alerts are stupid');
+// }
+
+// stupidAlert();
+
+document.getElementById('title').style.color='red';
+
+// document.getElementById('hello').html('goodbye');
+
+// hello.html='cool';
