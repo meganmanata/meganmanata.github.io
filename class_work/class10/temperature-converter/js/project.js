6fe82d463432e153b1e50c9ebd784487384f1bf7
meganmanata <meganmanata@gmail.com>
2017-01-23 18:38:13 -0500

    converter commit 5
    made a CtoF button. Its a little uglier now, but I can't exactly figure
    out how to get it right. :-( Also cleaned up the sort of bad shopping I
    did on the arms.

--- class_work/class10/temperature-converter/js/project.js
+++ class_work/class10/temperature-converter/js/project.js
@@ -27,6 +27,35 @@ function attack () {
 
 }
 
+$('#fightCtoF').click(attack)
+
+function attack () {
+	
+	// console.log(attack)
+	$('.guys').attr('src', 'img/sci_fight_web.png');
+
+	var inp = $('.right').val();
+	//$('.left').val(parseInt(inp));
+	
+	var out;
+
+	console.log(inp, isNaN(inp));
+	console.log(isNaN(" "))
+
+	if (isNaN(parseFloat(inp))){
+		out = "Error!";
+	}
+
+	else{
+		out = (inp*1.8)+32;
+		out = out.toFixed(2);
+	}
+
+	$('.left').val( out );
+
+
+}
+
 $('#clear').click(reset)
 
 function reset () {
