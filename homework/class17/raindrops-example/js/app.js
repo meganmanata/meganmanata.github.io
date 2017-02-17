f1c50154ad7e4c071f349a3970af8fdefddcff98
meganmanata <meganmanata@gmail.com>
2017-02-17 12:24:16 -0500

    class 17 things

---
+++ homework/class17/raindrops-example/js/app.js
@@ -0,0 +1,16 @@
+// $(document).ready(function () {
+
+// $('.box').raindrops({
+// color:'#99d4a5',
+// canvasHeight:200});
+
+// })
+
+jQuery('.box').raindrops(
+{color:'#f77b7b',
+canvasHeight:150, 
+waveLength: 100,
+ rippleSpeed: 0.05, 
+density: 0.04});
+
+console.log('working');
