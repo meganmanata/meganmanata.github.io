39356d9c0bd5fcb67c5b03c02ac0400eb2bbb80b
meganmanata <meganmanata@gmail.com>
2017-02-24 02:10:46 -0500

    final project commit 14
    added a map and some cute colors :-)

---
+++ final project/js/map.js
@@ -0,0 +1,105 @@
+var map;
+  
+function initMap() {
+  map = new google.maps.Map(document.getElementById('map'), {
+      center: {lat: 40.754931, lng: -73.984019}, 
+      zoom: 15,
+      styles: [
+  {
+    "featureType": "landscape",
+    "stylers": [
+      {
+        "color": "#f7ead7"
+      }
+    ]
+  },
+  {
+    "featureType": "poi",
+    "stylers": [
+      {
+        "color": "#e31d19"
+      },
+      {
+        "visibility": "simplified"
+      }
+    ]
+  },
+  {
+    "featureType": "poi",
+    "elementType": "labels.text",
+    "stylers": [
+      {
+        "color": "#000000"
+      }
+    ]
+  },
+  {
+    "featureType": "poi",
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "color": "#000000"
+      }
+    ]
+  },
+  {
+    "featureType": "poi",
+    "elementType": "labels.text.stroke",
+    "stylers": [
+      {
+        "color": "#000000"
+      }
+    ]
+  },
+  {
+    "featureType": "road",
+    "stylers": [
+      {
+        "color": "#deac11"
+      }
+    ]
+  },
+  {
+    "featureType": "road",
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "color": "#efc034"
+      }
+    ]
+  },
+  {
+    "featureType": "road",
+    "elementType": "labels.text",
+    "stylers": [
+      {
+        "color": "#000000"
+      }
+    ]
+  },
+  {
+    "featureType": "road",
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "visibility": "off"
+      }
+    ]
+  }
+]
+   });
+
+// var image = '../img/FamousAmadeus-Logo.jpg';
+var marker = new google.maps.Marker({
+      position: {lat: 40.762408, lng: -73.985378}, 
+      map: map
+      // icon: image
+    });
+
+var marker = new google.maps.Marker({
+      position: {lat: 40.749819, lng: -73.994811}, 
+      map: map
+    });
+
+
+ }
